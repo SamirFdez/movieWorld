042e8957ba9fc4df329685b86a62fb489258820d
Samir <Samirfdezc@gmail.com>
2024-01-02 23:03:54 -0400

    ++

--- src/components/infoViewMovie/carouselMovieSimilar.jsx
+++ src/components/infoViewMovie/carouselMovieSimilar.jsx
@@ -13,23 +13,27 @@ export const CarouselMovieSimilar = ({ movieSimilar }) => {
   return (
     <>
       <div className="container p-5 mx-auto">
-      <div className="lg:w-4/5 mx-auto">
-
-        <Carousel {...carouselOptions}>
-          {movieSimilar?.filter((movie => movie.poster_path !== null)).map((moviesSimilar, index) => (
-            <div
-              className="shadow-md shadow-gray-800 transition-all duration-700 hover:scale-105 cursor-pointer mx-1 my-5"
-              key={`movies-by-genre-${index + 1}`}
-              onClick={() => goToInfoView(moviesSimilar.id)}
-            >
-              <img
-                src={`https://image.tmdb.org/t/p/w400${moviesSimilar.poster_path}`}
-                alt={moviesSimilar.original_title}
-                className="rounded-lg  h-60  lg:h-80 object-fill object-top"
-              />
-            </div>
-          ))}
-        </Carousel>
+        <div className="lg:w-4/5 mx-auto">
+          <h3 className="text-3xl font-bold dark:text-white tracking-wider">
+            Similar Movies
+          </h3>
+          <Carousel {...carouselOptions}>
+            {movieSimilar
+              ?.filter((movie) => movie.poster_path !== null)
+              .map((moviesSimilar, index) => (
+                <div
+                  className="shadow-md shadow-gray-800 transition-all duration-700 hover:scale-105 cursor-pointer mx-1 my-5"
+                  key={`movies-by-genre-${index + 1}`}
+                  onClick={() => goToInfoView(moviesSimilar.id)}
+                >
+                  <img
+                    src={`https://image.tmdb.org/t/p/w400${moviesSimilar.poster_path}`}
+                    alt={moviesSimilar.original_title}
+                    className="rounded-lg  h-60  lg:h-80 object-fill object-top"
+                  />
+                </div>
+              ))}
+          </Carousel>
         </div>
       </div>
     </>
