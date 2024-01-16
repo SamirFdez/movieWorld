f0c59d44e2023ae21796afd8602f27417cb81e94
Samir Fernandez <samirfdezc@gmail.com>
2024-01-16 18:00:07 -0400

    ++

--- src/components/movies/moviesResults.jsx
+++ src/components/movies/moviesResults.jsx
@@ -4,7 +4,7 @@ import { MovieCard } from "../Cards/movieCard";
 export const MoviesResults = ({ dataMovie }) => {
   return (
     <>
-      <div className="container mx-auto" style={{ marginTop: "70px" }}>
+      <div className="container mx-auto">
         <div className="flex flex-wrap m-4">
           {dataMovie?.map((data, index) => (
             <MovieCard data={data} key={`movie-card-${index}`} />
